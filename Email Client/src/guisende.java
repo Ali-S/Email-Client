307f5e462c643179231f5a68a1625a7c7ea7c567
furkanyucel <furkanyucel@192.168.2.104>
2015-01-25 01:49:31 +0100

    aa

--- Email Client/src/guisende.java
+++ Email Client/src/guisende.java
@@ -1,36 +1,85 @@
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
-import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+
 
 
 public class guisende {
+	
+	String message; 
+	
 	public guisende(){
-		JFrame fenster = new JFrame("Send E-Mail");
-		fenster.getContentPane().setLayout(new BorderLayout());
-		JTextArea textarea = new JTextArea();
-		textarea.setSize(400, 300);
+		JFrame frame = new JFrame("New E-Mail");
+		frame.setLocationRelativeTo(null);
+		frame.setSize(800,600);
+		
+		
 		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
-		panel.setSize(400, 100);
-		JLabel to = new JLabel("To: ");
-		panel.add(to);
-		to.setAlignmentX(to.CENTER_ALIGNMENT);
-		JTextArea textto = new JTextArea();
-		to.add(textto);
-		JLabel subject = new JLabel("Subject: ");
-		panel.add(subject);
-		to.setAlignmentX(subject.CENTER_ALIGNMENT);
+		GroupLayout layout = new GroupLayout(panel);
+		panel.setSize(300,100);
+		panel.setLayout(layout);
 		
-		fenster.add(panel, BorderLayout.NORTH);
-		fenster.add(textarea, BorderLayout.CENTER);
+		JLabel lto = new JLabel("TO:");
+		panel.add(lto);
 		
-		fenster.setSize(600, 600);
-		fenster.setLocationRelativeTo(null);
-		fenster.setVisible(true);
+		JTextField tfto = new JTextField("",15);
+		panel.add(tfto);
 		
+		JLabel lsubject = new JLabel("Subject:");
+		panel.add(lsubject);
+		
+		JTextField tfsubject = new JTextField("",15);
+		panel.add(tfsubject);
+		
+		layout.setVerticalGroup(
+				layout.createSequentialGroup()
+				 	.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				 			.addComponent(lto).addComponent(tfto))
+				 	.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				 			.addComponent(lsubject).addComponent(tfsubject)));
+		
+		layout.setHorizontalGroup(
+				layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addComponent(lto).addComponent(lsubject))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(tfto).addComponent(tfsubject)));
+		
+		
+		
+		JToolBar tbar = new JToolBar();
+		tbar.add(new JButton("Test"));
+		tbar.add(new JButton("Made"));
+		tbar.setSize(300, 10);
+		
+		JEditorPane editor = new JEditorPane();
+		editor.setContentType("text/html");
+		editor.setEditable(true);
+		
+		JScrollPane scrollPane = new JScrollPane(editor);
+		
+		message = editor.getText();
+		
+		
+		
+		
+		frame.add(panel);
+		frame.add(tbar);
+		frame.add(scrollPane);
+		frame.setLayout(new GridLayout(3,0));
+		frame.setVisible(true);
 	}
 }
