991aea9aba3c89fc19263d6fe1fc99f7b9fb903e
furkanyucel <furkanyucel@192.168.2.106>
2015-02-11 22:47:03 +0100

    kkk

--- Email Client/src/start.java
+++ Email Client/src/start.java
@@ -14,9 +14,8 @@ public class start {
 	static String title = "Java E-Mail-Client Connection Init Beta v.02";
 	
 	public static void main(String[] args) throws IOException {
-		// Teste anmelden Fenster
-//		new anmelden("Please enter your E-Mail Server!");
-		new HauptGUI(title);
+		new anmelden("Please enter your E-Mail Server!");
+		
 	
 	}
 }
